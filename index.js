cce36b57486ec8196d20d8139636b3e4000f67cc
ManoharNaidu <beesettim27@gmail.com>
2024-03-07 17:08:15 +0530

    Added post request and successfully handled files

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const fileupload = require('express-fileupload');
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -7,12 +7,22 @@ app.set('view engine', 'ejs');
 // middlewares
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(fileupload({
+    useTempFiles: true,
+    tempFileDir: '/tmp/'
+}));
 
 
 app.get("/simpleGet",(req,res) => {
     console.log(req.query);
 
     res.send(req.query);
+})
+
+app.post("/simplePost",(req,res) => {
+    console.log(req.body);
+    console.log(req.files);
+    res.send({"body" : req.body, "files" : req.files});
 })
 
 app.get("/getForm",(req,res) => {

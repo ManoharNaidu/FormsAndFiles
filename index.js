dacda32dd11353dbacc6df9c91b5382a6468cd76
ManoharNaidu <beesettim27@gmail.com>
2024-03-07 18:15:36 +0530

    Now can upload any file(one at a time) to the cloudinary cloud using POST method

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const express = require('express');
 const fileupload = require('express-fileupload');
+const cloudinary = require('cloudinary').v2;
+
+cloudinary.config({
+    // cloud_name: process,env,cloud_name, its for the production part
+    cloud_name: 'ds33dm8cf',
+    api_key : '351148593964452',
+    api_secret: 'vyYYX7BZaXWN40QKNLZyKSGez7I',
+    secure: true
+})
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.set('view engine', 'ejs');
@@ -18,14 +28,24 @@ app.get("/",(req,res) => {
 
 app.get("/simpleGet",(req,res) => {
     console.log(req.query);
-
     res.send(req.query);
 })
 
-app.post("/simplePost",(req,res) => {
+app.post("/simplePost", async(req,res) => {
     console.log(req.body);
     console.log(req.files);
-    res.send({"body" : req.body, "files" : req.files});
+    let file = req.files.sampleFile;
+    const result = await cloudinary.uploader.upload(file.tempFilePath, {
+        folder : "User"
+    })
+    console.log(result);
+
+    details = {
+        firstname : req.body.firstname,
+        lastname : req.body.lastname,
+        result : result,
+    }
+    res.send(details);
 })
 
 app.get("/getForm",(req,res) => {
